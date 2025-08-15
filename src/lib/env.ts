c56055127c277f8606152bb553bc0cd9c0e13e90
MARK XPLORER <25markmarket@gmail.com>
2025-08-15 10:22:20 +0700

    Update env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,13 +1,13 @@
 // Environment configuration for PayLite Self-Report
 export const ENV = {
   OWNER_WHATSAPP: "6285883795285",
-  MERCHANT_NAME: "Mark Xplorer",
+  MERCHANT_NAME: "CRAZY STORE",
   RECEIPT_SIGN_SECRET: "paylite-secret-key-2024",
   PAYMENT_ACCOUNT_BANK: "BCA 123456789 a/n Mark Xplorer",
-  PAYMENT_ACCOUNT_EWALLET: "@markxplorer",
-  QRIS_STATIC_IMAGE_URL: "https://placeholder.co/300x300/00FFFF/000000?text=QRIS+CODE",
+  PAYMENT_ACCOUNT_EWALLET: "085775436135",
+  QRIS_STATIC_IMAGE_URL: "https://telegra.ph/file/3eb51c3f6dccae3c9502c.png",
   SAWERIA_URL: "https://saweria.co/markxplorer",
-  SOCIAL_INSTAGRAM: "https://instagram.com/markxplorer",
+  SOCIAL_INSTAGRAM: "https://instagram.com/_markxplorer.io",
   SOCIAL_TELEGRAM: "https://t.me/markxplorer",
   SOCIAL_GITHUB: "https://github.com/markxplorer",
   BASE_URL: "https://paylite-report.vercel.app"
